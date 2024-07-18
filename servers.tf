d17e8a58d7c56c4c59962432792a78ee331d5fce
girish <girishiva1989@gmail.com>
2024-07-18 14:55:23 +1000

    Add function

--- servers.tf
+++ servers.tf
@@ -21,6 +21,15 @@ output "frontend" {
   value = aws_instance.frontend.public_ip
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = Z014155316GQK32DIY108
+  name    = "frontend-dev.gdevops89.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
+}
+
+
 resource "aws_instance" "catalogue" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.small"
@@ -28,6 +37,14 @@ resource "aws_instance" "catalogue" {
   tags = {
     Name = "catalogue"
   }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = Z014155316GQK32DIY108
+  name    = "catalogue-dev.gdevops89.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
 }
 
 resource "aws_instance" "mongod" {
@@ -39,6 +56,14 @@ resource "aws_instance" "mongod" {
   }
 }
 
+resource "aws_route53_record" "mongod" {
+  zone_id = Z014155316GQK32DIY108
+  name    = "mongod-dev.gdevops89.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "redis" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.small"
@@ -46,6 +71,14 @@ resource "aws_instance" "redis" {
   tags = {
     Name = "redis"
   }
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = Z014155316GQK32DIY108
+  name    = "redis-dev.gdevops89.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
 }
 
 resource "aws_instance" "user" {
@@ -57,6 +90,14 @@ resource "aws_instance" "user" {
   }
 }
 
+resource "aws_route53_record" "user" {
+  zone_id = Z014155316GQK32DIY108
+  name    = "user-dev.gdevops89.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "cart" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.small"
@@ -66,7 +107,15 @@ resource "aws_instance" "cart" {
   }
 }
 
-resource "aws_instance" "mysql" {
+resource "aws_route53_record" "cart" {
+  zone_id = Z014155316GQK32DIY108
+  name    = "mongod-dev.gdevops89.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
+}
+
+resource "aws_instance" "cart" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.small"
 
@@ -84,6 +133,14 @@ resource "aws_instance" "shipping" {
   }
 }
 
+resource "aws_route53_record" "shipping" {
+  zone_id = Z014155316GQK32DIY108
+  name    = "shipping-dev.gdevops89.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "rabbitmq" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.small"
@@ -93,6 +150,14 @@ resource "aws_instance" "rabbitmq" {
   }
 }
 
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = Z014155316GQK32DIY108
+  name    = "rabbitmq-dev.gdevops89.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "payment" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.small"
@@ -100,4 +165,12 @@ resource "aws_instance" "payment" {
   tags = {
     Name = "Payment"
   }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = Z014155316GQK32DIY108
+  name    = "payment-dev.gdevops89.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
 }
