a2405f9661ddd3ed030dcf311f50c98e8d69c3e7
girish <girishiva1989@gmail.com>
2024-07-18 13:34:46 +1000

    Add function

--- servers.tf
+++ servers.tf
@@ -6,3 +6,84 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
+
+resource "aws_instance" "catalogue" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+resource "aws_instance" "mongod" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "mongod"
+  }
+}
+
+resource "aws_instance" "redis" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "redis"
+  }
+}
+
+resource "aws_instance" "user" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "user"
+  }
+}
+
+resource "aws_instance" "cart" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "cart"
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_instance" "shipping" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "shipping"
+  }
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+resource "aws_instance" "payment" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "Payment"
+  }
+}
