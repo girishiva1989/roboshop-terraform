a9ba6c26c81c01536e2ff9cd7994efa66665a6d5
girish <girishiva1989@gmail.com>
2024-07-21 22:57:13 +1000

    Add function

--- servers.tf
+++ servers.tf
@@ -17,32 +17,84 @@ data "aws_security_group" "allow-all"{
   name = "allow-all"
 }
 
-variable "instance_type" {
-  default = "t3.small"
-}
-
 variable "components" {
-  default = ["frontend","mongod","catalogue","redis","user","cart","mysql","shipping","rabbitmq",
-  "payment"]
+  default = {
+    frontend = {
+      name          = "fronend"
+      instance_type = "t3.small"
+    }
+    mongod = {
+      name          = "mongod"
+      instance_type = "t3.small"
+    }
+    catalogue = {
+      name          = "catalogue"
+      instance_type = "t3.small"
+    }
+    redis = {
+      name          = "redis"
+      instance_type = "t3.small"
+    }
+    user = {
+      name          = "user"
+      instance_type = "t3.small"
+    }
+    cart = {
+      name          = "cart"
+      instance_type = "t3.small"
+    }
+    mysql = {
+      name          = "mysql"
+      instance_type = "t3.small"
+    }
+    shipping = {
+      name          = "shipping"
+      instance_type = "t3.small"
+    }
+    rabbitmq = {
+      name          = "rabbitmq"
+      instance_type = "t3.small"
+    }
+    payment = {
+      name          = "payment"
+      instance_type = "t3.small"
+    }
+  }
 }
 
 resource "aws_instance" "instances" {
-  #count         = length(var.components)
   for_each      = toset(var.components)
   ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
+  instance_type = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
     #Name = var.components[count.index]
-    Name = each.key
+    Name = each.value["name"]
   }
 }
 
-output "instance" {
-  value = [for i in aws_instance.instances : i.public_ip]
-#   value = var.components[each.key]
-}
+# variable "components" {
+#   default = ["frontend","mongod","catalogue","redis","user","cart","mysql","shipping","rabbitmq",
+#   "payment"]
+# }
+#
+# resource "aws_instance" "instances" {
+#   #count         = length(var.components)
+#   for_each      = toset(var.components)
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+#
+#   tags = {
+#     #Name = var.components[count.index]
+#     Name = each.key
+#   }
+# }
+#
+# output "instance" {
+#   value = [for i in aws_instance.instances : i.public_ip]
+# }
 
 # resource "aws_route53_record" "instance" {
 #   zone_id = data.aws_route53_zone.zone.zone_id
@@ -51,157 +103,3 @@ output "instance" {
 #   ttl     = 300
 #   records = [aws_instance.instances.private_ip]
 # }
-
-
-# resource "aws_instance" "catalogue" {
-#   ami           = data.aws_ami.centos.image_id
-#   instance_type = var.instance_type
-#
-#   tags = {
-#     Name = "catalogue"
-#   }
-# }
-#
-# resource "aws_route53_record" "catalogue" {
-#   zone_id = "Z014155316GQK32DIY108"
-#   name    = "catalogue-dev.gdevops89.online"
-#   type    = "A"
-#   ttl     = 300
-#   records = [aws_instance.frontend.private_ip]
-# }
-#
-# resource "aws_instance" "mongod" {
-#   ami           = data.aws_ami.centos.image_id
-#   instance_type = var.instance_type
-#
-#   tags = {
-#     Name = "mongod"
-#   }
-# }
-#
-# resource "aws_route53_record" "mongod" {
-#   zone_id = "Z014155316GQK32DIY108"
-#   name    = "mongod-dev.gdevops89.online"
-#   type    = "A"
-#   ttl     = 300
-#   records = [aws_instance.frontend.private_ip]
-# }
-#
-# resource "aws_instance" "redis" {
-#   ami           = data.aws_ami.centos.image_id
-#   instance_type = var.instance_type
-#
-#   tags = {
-#     Name = "redis"
-#   }
-# }
-#
-# resource "aws_route53_record" "redis" {
-#   zone_id = "Z014155316GQK32DIY108"
-#   name    = "redis-dev.gdevops89.online"
-#   type    = "A"
-#   ttl     = 300
-#   records = [aws_instance.frontend.private_ip]
-# }
-#
-# resource "aws_instance" "user" {
-#   ami           = data.aws_ami.centos.image_id
-#   instance_type = var.instance_type
-#
-#   tags = {
-#     Name = "user"
-#   }
-# }
-#
-# resource "aws_route53_record" "user" {
-#   zone_id = "Z014155316GQK32DIY108"
-#   name    = "user-dev.gdevops89.online"
-#   type    = "A"
-#   ttl     = 300
-#   records = [aws_instance.frontend.private_ip]
-# }
-#
-# resource "aws_instance" "cart" {
-#   ami           = data.aws_ami.centos.image_id
-#   instance_type = var.instance_type
-#
-#   tags = {
-#     Name = "cart"
-#   }
-# }
-#
-# resource "aws_route53_record" "cart" {
-#   zone_id = "Z014155316GQK32DIY108"
-#   name    = "cart-dev.gdevops89.online"
-#   type    = "A"
-#   ttl     = 300
-#   records = [aws_instance.frontend.private_ip]
-# }
-#
-# resource "aws_instance" "mysql" {
-#   ami           = data.aws_ami.centos.image_id
-#   instance_type = var.instance_type
-#
-#   tags = {
-#     Name = "mysql"
-#   }
-# }
-#
-# resource "aws_route53_record" "mysql" {
-#   zone_id = "Z014155316GQK32DIY108"
-#   name    = "mysql-dev.gdevops89.online"
-#   type    = "A"
-#   ttl     = 300
-#   records = [aws_instance.frontend.private_ip]
-# }
-#
-# resource "aws_instance" "shipping" {
-#   ami           = data.aws_ami.centos.image_id
-#   instance_type = var.instance_type
-#
-#   tags = {
-#     Name = "shipping"
-#   }
-# }
-#
-# resource "aws_route53_record" "shipping" {
-#   zone_id = "Z014155316GQK32DIY108"
-#   name    = "shipping-dev.gdevops89.online"
-#   type    = "A"
-#   ttl     = 300
-#   records = [aws_instance.frontend.private_ip]
-# }
-#
-# resource "aws_instance" "rabbitmq" {
-#   ami           = data.aws_ami.centos.image_id
-#   instance_type = "t3.small"
-#
-#   tags = {
-#     Name = var.instance_type
-#   }
-# }
-#
-# resource "aws_route53_record" "rabbitmq" {
-#   zone_id = "Z014155316GQK32DIY108"
-#   name    = "rabbitmq-dev.gdevops89.online"
-#   type    = "A"
-#   ttl     = 300
-#   records = [aws_instance.frontend.private_ip]
-# }
-#
-# resource "aws_instance" "payment" {
-#   ami           = data.aws_ami.centos.image_id
-#   instance_type = var.instance_type
-#
-#   tags = {
-#     Name = "Payment"
-#   }
-# }
-#
-# resource "aws_route53_record" "payment" {
-#   zone_id = "Z014155316GQK32DIY108"
-#   name    = "payment-dev.gdevops89.online"
-#   type    = "A"
-#   ttl     = 300
-#   records = [aws_instance.frontend.private_ip]
-#}
