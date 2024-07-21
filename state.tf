c35ebff3a318280ede35b5c2fe814746f14e8721
girish <girishiva1989@gmail.com>
2024-07-22 00:20:50 +1000

    Add function

---
+++ state.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-72"
+    key            = "roboshop/dev/terraform.tfstate" # Define a unique path for your state file
+    region         = "us-east-1"
+  }
+}
