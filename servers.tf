7e72368b506de845c1272702d59e094bc00ca7f5
girish <girishiva1989@gmail.com>
2024-07-21 23:17:40 +1000

    Add function

--- servers.tf
+++ servers.tf
@@ -73,18 +73,18 @@ resource "aws_instance" "instances" {
   }
 }
 
-output "instance" {
-  value = aws_instance.instances[each.value["name"]].public_ip
-}
-
-# resource "aws_route53_record" "instance" {
-#   for_each = var.components
-#   zone_id = data.aws_route53_zone.zone.zone_id
-#   name    = "frontend-dev.gdevops89.online"
-#   type    = "A"
-#   ttl     = 300
-#   records = [aws_instance.instances[each.value["name"]].private_ip]
+# output "instance" {
+#   value = aws_instance.instances[each.value["name"]].public_ip
 # }
+
+resource "aws_route53_record" "instance" {
+  for_each = var.components
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "frontend-dev.gdevops89.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.instances[each.value["name"]].private_ip]
+}
 
 
 # variable "components" {
