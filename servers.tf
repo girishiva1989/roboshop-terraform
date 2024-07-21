b329a976b38bc897a2e4585ecce9f7a327c6728f
girish <girishiva1989@gmail.com>
2024-07-21 23:01:05 +1000

    Add function

--- servers.tf
+++ servers.tf
@@ -63,7 +63,7 @@ variable "components" {
 }
 
 resource "aws_instance" "instances" {
-  for_each      = toset(var.components)
+  for_each      = var.components
   ami           = data.aws_ami.centos.image_id
   instance_type = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
