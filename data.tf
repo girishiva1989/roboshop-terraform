5cc25163c323d43853a6696db52decd0b91eb8ba
girish <girishiva1989@gmail.com>
2024-07-23 12:05:21 +1000

    Add function

---
+++ data.tf
@@ -0,0 +1,14 @@
+data "aws_ami" "centos" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners = ["973714476881"]
+}
+
+data "aws_route53_zone" "zone" {
+  name = "gdevops89.online"
+}
+
+
+data "aws_security_group" "allow-all"{
+  name = "allow-all"
+}
